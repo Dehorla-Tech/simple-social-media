d88e7c39ebb1444a425686fffec65aaa4a70102e
Dehorla-Tech <dehorladesign0001:gmail.com>
2024-10-10 05:32:44 +0100

    updated posts and user routes

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,26 +2,36 @@ import express, { Router } from "express";
 const router = express.Router();
 
 import {register, login} from "../controllers/auth.js";
-import { userProfile, profiles, update, } from "../controllers/user.js";
-import { post, getPost, allPost } from "../controllers/post.js";
+import { userProfile, allProfiles, updateUser, deleteUser} from "../controllers/user.js";
+import { createPost, getPost, allPost, updatePost, deletePost } from "../controllers/post.js";
  
 
-
+    //signup route
 router.route("/register").post(register);
 
 router.route("/login").post(login);
 
-router.route("/profiles").get(profiles);
+      //user route
+router.route("/allProfiles").get(allProfiles);
 
 router.route("/userProfile/:id").get(userProfile);
 
-router.route("/update/:id").put(update);
+router.route("/updateUser/:id").put(updateUser);
 
+router.route("/deleteUser/:id").delete(deleteUser);
+
+     //post route
 router.route("/allPost").get(allPost);
 
-router.route("/post").post(post);
+router.route("/createPost").post(createPost);
 
 router.route("/getPost/:id").get(getPost);
+
+router.route("/updatePost/:id").put(updatePost);
+
+router.route("/deletePost/:id").delete(deletePost);
+
+
 
 
 export {router};
